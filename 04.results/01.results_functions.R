109ff140952d43c03ea0e7a15779e826695e7be2
noeliarico <noeliarico@uniovi.es>
2020-01-21 08:20:32 +0100

    Add functions to export latex

--- 04.results/01.results_functions.R
+++ 04.results/01.results_functions.R
@@ -4,7 +4,7 @@
 # errors computed which each of the distance
 calculate_errors <- function(results_of_one_dataset) {
   errors_one_dataset <- sapply(results_of_one_dataset, function(x) x$tot.withinss)
-  rownames(errors_one_dataset) <- c(paste0("error_d", selected_distances))
+  rownames(errors_one_dataset) <- c(paste0("error_d", distances[selected_distances]))
   errors_one_dataset
   #colSums(errorsd1)
 }
